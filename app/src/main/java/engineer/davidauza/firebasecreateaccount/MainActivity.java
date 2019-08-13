285b38d5fff2d73b403c58184d43adc380feec99
davidauza.engineer <davidauza01@gmail.com>
2019-08-12 20:42:32 -0500

    Set phone EditText

--- app/src/main/java/engineer/davidauza/firebasecreateaccount/MainActivity.java
+++ app/src/main/java/engineer/davidauza/firebasecreateaccount/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.KeyEvent;
 import android.widget.TextView;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         setUpNameEditText();
         setUpEmailEditText();
+        setUpPhoneEditText();
     }
 
     /**
@@ -91,6 +93,39 @@ public class MainActivity extends AppCompatActivity {
      */
     private boolean checkEmail(CharSequence pEmailEntered) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(pEmailEntered).matches();
+    }
+
+    /**
+     * This method configures the behavior of the phone EditText.
+     */
+    private void setUpPhoneEditText() {
+        TextInputEditText phoneEditText = findViewById(R.id.txt_phone);
+        final TextInputLayout PHONE_LAYOUT = findViewById(R.id.ly_phone);
+        // Listener for the action method on the editor
+        phoneEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (checkPhone(v.getText())) {
+                    if (PHONE_LAYOUT != null) {
+                        PHONE_LAYOUT.setError(null);
+                    }
+                } else {
+                    PHONE_LAYOUT.setError(getString(R.string.main_activity_error_phone));
+                }
+                return false;
+            }
+        });
+    }
+
+    /**
+     * This method checks if a CharSequence is built with a phone format.
+     *
+     * @param pPhoneEntered is the CharSequence to check.
+     * @return true if the CharSequence contains a text with a phone format, otherwise it returns
+     * false.
+     */
+    private boolean checkPhone(CharSequence pPhoneEntered) {
+        return Patterns.PHONE.matcher(pPhoneEntered).matches();
     }
 
 }
